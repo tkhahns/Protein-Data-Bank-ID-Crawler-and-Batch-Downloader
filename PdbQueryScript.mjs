677c837a4c777ec20201a761ddee21eb9f0f9e62
Siwan Li <siwanli1337@gmail.com>
2024-02-21 00:14:29 +0800

    Program now gets all ids

--- PdbQueryScript.mjs
+++ PdbQueryScript.mjs
@@ -1,17 +1,36 @@
 import fetch from 'node-fetch';
 
+const numOfStructures = 215908;
+const pageLimit = 10000;
+const numOfPages = Math.floor(215908/100);
 const baseUrl = 'https://search.rcsb.org/rcsbsearch/v2/query';
 
-const data = encodeURIComponent(
-    JSON.stringify({
-        query: {
-            type: "terminal",
-            service: "text"
-            },
-            return_type: "entry"
-    }))
-const url = baseUrl + `?json=${data}`
+//let startNumbers = Array.from({length: numOfPages}, (_, i) => i * pageLimit);
+let ids = [];
+let data = {
+    query: {
+        type: "terminal",
+        service: "text"
+    },
+    request_options: {
+        paginate: {
+            start: 0,
+            rows: pageLimit
+        }
+    },
+    return_type: "entry"
+};
 
-fetch(url)
-    .then(res => res.text())
-    .then(text => console.log(text));
+// correct the number of loops
+const iterations = Math.floor(numOfStructures/pageLimit);
+for (let i = 0; i < 2; i++) {
+    data.request_options.paginate.start = i * pageLimit;
+    const url = baseUrl + `?json=${encodeURIComponent(JSON.stringify(data))}`;
+
+    let currentIds = await fetch(url)
+        .then(res => res.json())
+        .then(json => json["result_set"].map(protein => protein["identifier"]));
+    
+    ids = ids.concat(currentIds);
+}
+console.log(ids);
